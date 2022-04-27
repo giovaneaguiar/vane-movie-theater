647bc4ddfb5807bb3b6b94489179f9cfe7b80580
Giovane Aguiar <giovaneaguiar@ice.ufjf.br>
2022-04-27 20:25:31 -0300

    Criando a barra de navegação.

---
+++ galeria.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <title>Vane Movie Theater</title>
+</head>
+<body>
+<nav class="nav-extended brown darken-2">
+    <div class="nav-wrapper">
+        <ul id="nav-mobile" class="right">
+            <li><a href="galeria.php">Galeria</a></li>
+            <li><a href="cadastrar.php">Cadastrar</a></li>
+        </ul> 
+    </div>
+    <div class="nav-header center">    
+        <h1>Vane Movie Theater</h1>
+    </div>
+    <div class="nav-content">
+      <ul class="tabs tabs-transparent brown darken-4">
+        <li class="tab"><a class="active" href="#test1">Todos</a></li>
+        <li class="tab"><a href="#test2">Assistidos</a></li>
+        <li class="tab"><a href="#test3">Favoritos</a></li>
+      </ul>
+    </div>
+  </nav>
+  
+</body>
+</html>
--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-echo "Início do Projeto.";
-
-?>
