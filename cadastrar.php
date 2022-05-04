11651a7d3b986e2db4df3b3f1519b92fbfb5961d
Giovane Aguiar <giovaneaguiar@ice.ufjf.br>
2022-05-03 22:48:52 -0300

    Criação do formulário e validações de inputs.

--- cadastrar.php
+++ cadastrar.php
@@ -37,6 +37,43 @@
             <div class="card">
                 <div class="card-content">
                     <span class="card-title">Cadastrar Filme</span>
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <input id="titulo" type="text" class="validate" require>
+                            <label for="titulo">Título do Filme</label>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <form class="col s12">
+                            <div class="row">
+                                <div class="input-field col s12">
+                                    <textarea id="sinopse" class="materialize-textarea"></textarea>
+                                    <label for="sinopse">Sinopse</label>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+
+                    <div class="row">
+                        <div class="input-field col s4">
+                            <input id="nota" type="number" step=".1" min=0 max=10 class="validate" require>
+                            <label for="nota">Nota</label>
+                        </div>
+                    </div>
+
+                    <form action="#">
+                        <div class="file-field input-field">
+                            <div class="btn brown lighten-2">
+                                <span>Capa</span>
+                                <input type="file">
+                            </div>
+                            <div class="file-path-wrapper">
+                                <input class="file-path validate" type="text">
+                            </div>
+                        </div>
+                    </form>
+
                     <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
                 </div>
                 <div class="card-action">
