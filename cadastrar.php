ffb019d1b5b23eeb8310caa4683216658df678e8
Giovane Aguiar <giovaneaguiar@ice.ufjf.br>
2022-05-03 22:28:48 -0300

    Criação de card e botões de ação no formulário.

---
+++ cadastrar.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- Compiled and minified JavaScript -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <title>Vane Movie Theater</title>
+</head>
+
+<body>
+    <nav class="nav-extended brown darken-2">
+        <div class="nav-wrapper">
+            <ul id="nav-mobile" class="right">
+                <li><a href="galeria.php">Galeria</a></li>
+                <li><a href="cadastrar.php">Cadastrar</a></li>
+            </ul>
+        </div>
+        <div class="nav-header center">
+            <h1>Vane Movie Theater</h1>
+        </div>
+        <div class="nav-content">
+            <ul class="tabs tabs-transparent brown darken-4">
+                <li class="tab"><a class="active" href="#test1">Todos</a></li>
+                <li class="tab"><a href="#test2">Assistidos</a></li>
+                <li class="tab"><a href="#test3">Favoritos</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="row">
+        <div class="col s6 offset-s3">
+            <div class="card">
+                <div class="card-content">
+                    <span class="card-title">Cadastrar Filme</span>
+                    <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                </div>
+                <div class="card-action">
+                    <a class="btn waves-effect waves-light grey" href="galeria.php">Cancelar</a>
+                    <a href="#" class="waves-effect waves-light btn brown">Confirmar</a>
+                </div>
+            </div>
+
+        </div>
+
+
+
+
+
+    </div>
+
+</body>
