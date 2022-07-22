70a9cdbb3bad279ec6845dcd6f3d12747f790cec
Giovane Aguiar <giovaneaguiar@ice.ufjf.br>
2022-07-22 09:59:24 -0300

    Apagar filmes.

--- db/11072022_create_table_filmes.php
+++ db/11072022_create_table_filmes.php
@@ -17,42 +17,10 @@ $sql = "CREATE TABLE vanemovietheater (
     )
 ";
 
-/*if ($bd->exec($sql)) {
+if ($bd->exec($sql)) {
     echo "\nTabela vanemovietheater criada.\n";
 } else {
     echo "\nErro ao criar tabela vanemovietheater.\n";
 }
 
-$sql = "INSERT INTO vanemovietheater (id,titulo,poster,sinopse,nota) VALUES (
-    0, 
-    'Matrix',
-    'https://www.themoviedb.org/t/p/original/etJHvVsM9aefWWrW23r5BXgVK1F.jpg',
-    'Em um futuro próximo, Thomas Anderson (Keanu Reeves), um jovem programador de computador que mora em um cubículo escuro,
-    é atormentado por estranhos pesadelos nos quais encontra-se conectado por cabos e contra sua vontade, em um imenso sistema de computadores do futuro.
-    Em todas essas ocasiões, acorda gritando no exato momento em que os eletrodos estão para penetrar em seu cérebro.',
-    9.7
-)";
 
-if ($bd->exec($sql)) {
-    echo "\nFilmes inseridos com sucesso.\n";
-} else {
-    echo "\nErro ao inserir filmes.\n";
-}
-
-$sql = "INSERT INTO vanemovietheater (id,titulo,poster,sinopse,nota) VALUES (
-    1, 
-    'Pulp Fiction: Tempo de Violência',
-    'https://www.themoviedb.org/t/p/original/tptjnB2LDbuUWya9Cx5sQtv5hqb.jpg',
-    'Vincent Vega (John Travolta) e Jules Winnfield (Samuel L. Jackson) são dois assassinos profissionais que trabalham fazendo cobranças para Marsellus Wallace (Ving Rhames), 
-    um poderosos gângster. Vega é forçado a sair com a garota do chefe, temendo passar dos limites. Enquanto isso, o pugilista Butch Coolidge (Bruce Willis) 
-    se mete em apuros por ganhar uma luta que deveria perder.',
-    9.1
-)";
-
-if ($bd->exec($sql)) {
-    echo "\nFilmes inseridos com sucesso.\n";
-} else {
-    echo "\nErro ao inserir filmes.\n";
-}
-
-*/
